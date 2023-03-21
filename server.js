108a1694203b7763ee72661ad94506a1fea823ad
James Caleca <jacaleca2@gmail.com>
2023-03-21 11:53:38 -0600

    Server update for deployment

--- server.js
+++ server.js
@@ -13,9 +13,13 @@ const secret = process.env.SECRET
 app.use(express.json())
 app.use(morgan('dev'))
 
-mongoose.connect("mongodb://localhost:27017/rtv", 
-  () => console.log('connected to database')
-)
+// mongoose.connect("mongodb://localhost:27017/rtv", 
+//   () => console.log('connected to database')
+// )
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use(
   '/auth', 
@@ -30,7 +34,6 @@ app.use('/api', jwt({ secret: secret, algorithms: ['HS256'] }))
 
 app.use('/api/issues', require('./routes/issueRouter.js'))
 
-app.use(express.static(path.join(_dirname, "client", "build")))
 
 app.use((err, req, res, next) => {
   console.log(err)
@@ -40,9 +43,10 @@ app.use((err, req, res, next) => {
   return res.send({errMsg: err.message})
 })
 
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(_dirname, "client", "build", "index.html"))
-})
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(9000, () => {
   console.log("Server is running on port 9000")
